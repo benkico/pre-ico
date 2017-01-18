cf8ccd44dfc9d50b86150a997875062e2cc959b6
Benki <benkico@users.noreply.github.com>
2017-01-18 07:36:42 +0100

    Delete AbstractAscToken.sol

--- AbstractAscToken.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.2;
-import "AbstractToken.sol";
-
-contract AscToken is Token {
-    function issueTokens(address _for, uint tokenCount) payable returns (bool);
-    function changeEmissionContractAddress(address newAddress) returns (bool);
-}
